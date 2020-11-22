870229486f6ab2c72e61b570cea0c825a501a61d
devrcarlos <dev.rcarlos@gmail.com>
2020-11-22 17:00:57 -0300

    programação orientada a eventos

---
+++ programacao_orientada_eventos/exemplo_evento_1.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Button, Label
+from tkinter.messagebox import showinfo
+from time import strftime, localtime
+
+def clicked():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S%p\n', localtime())
+    showinfo(message=time)
+
+root = Tk()
+button = Button( root, text='Clique Aqui', command=clicked)
+button.pack()
+
+root.mainloop()
+
