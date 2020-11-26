45107e88e8cd949c4822ba3b405a609abb1e003d
devrcarlos <carlo@LATTOP-ACER.(none)>
2020-11-25 22:28:57 -0300

    teste

---
+++ gui_teste_01.py
@@ -0,0 +1,15 @@
+from tkinter import Tk, Label, PhotoImage, TOP, BOTTOM
+
+root = Tk()
+
+photo = PhotoImage(file='teste_gif.gif').subsample(5)
+
+img = Label(master=root, image=photo)
+img.pack(side=TOP)
+
+text = Label(master=root, font=('Courier', 18), text='Olá world!')
+text.pack(side=BOTTOM)
+
+img.pack()
+
+root.mainloop()
